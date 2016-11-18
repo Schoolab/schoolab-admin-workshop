a38f22da605ac0c510a0bd869cf0d9bf970a1449
pierredemilly <pierre.de.milly@gmail.com>
2016-11-18 09:57:58 +0000

    chaned events api controller

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -3,14 +3,14 @@ class Api::EventsController < ApplicationController
   # GET /events.json
   def index
     Time.zone = 'Paris'
-    @events = Event.where("datetime >= ?", Time.zone.today).order(datetime: :asc)
+    @events = Event.where("date >= ?", Time.zone.today).order(date: :asc)
     authorize! :read, Event
   end
 
   # GET /events/today.json
   def today
     Time.zone = 'Paris'
-    @events = Event.where(datetime: Time.zone.today).order(datetime: :asc)
+    @events = Event.where(date: Time.zone.today).order(start_time: :asc)
     authorize! :read, Event
   end
 
