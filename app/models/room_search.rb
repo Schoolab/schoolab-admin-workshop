0d81869bfd36bb852e47892b73a708e5aa90bacc
pierredemilly <pierre.de.milly@gmail.com>
2017-02-13 11:29:14 +0000

    new reservation form

--- app/models/room_search.rb
+++ app/models/room_search.rb
@@ -2,4 +2,12 @@ class RoomSearch < ApplicationRecord
 
 	date_time_attribute :start_time
 
+	def friendly_start_time
+		if self.start_time.present?
+			I18n.l(self.start_time, format: '%A %e %B %Y')
+		else
+			I18n.l(Date.today, format: '%A %e %B %Y')
+		end
+	end
+
 end
