165c3bddac9f7df9650db7180398b23d4310eb64
pierredemilly <pierre.de.milly@gmail.com>
2017-02-13 14:57:10 +0000

    removed weekday from reservation form input value

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,7 +35,7 @@ $(document).on('turbolinks:load', function() {
 		monthsShort: ["Jan", "Fev", "Mar", "Avr", "Mai", "Jui", "Jul", "Aou", "Sep", "Oct", "Nov", "Dec"],
 		today: "Aujourd'hui",
 		weekStart: 1,
-		format: "DD d MM yyyy",
+		format: "d MM yyyy",
 		titleFormat: 'MM yyyy'
 	};
 
