0de915c57ac8ef401eaf8922083c15f867e985fe
pierredemilly <pierre.de.milly@gmail.com>
2017-02-20 11:34:56 +0000

    added date to reservation serach results

--- app/models/room_search.rb
+++ app/models/room_search.rb
@@ -2,11 +2,19 @@ class RoomSearch < ApplicationRecord
 
 	date_time_attribute :start_time
 
-	def friendly_start_time
+	def datepicker_start_time
 		if self.start_time.present?
 			I18n.l(self.start_time, format: '%d/%m/%Y')
 		else
 			I18n.l(Date.today, format: '%d/%m/%Y')
+		end
+	end
+
+	def friendly_start_time
+		if self.start_time.present?
+			I18n.l(self.start_time, format: '%A %e %B %Y')
+		else
+			I18n.l(Date.today, format: '%A %e %B %Y')
 		end
 	end
 
