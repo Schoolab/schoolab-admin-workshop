2a2a320686f5dce63588a0a3fa7b8a520bc082f3
AlexandreJolly <accounts@alexandrejolly.com>
2017-02-21 16:05:49 +0100

    count resident in floors

--- app/models/floor.rb
+++ app/models/floor.rb
@@ -1,4 +1,19 @@
 class Floor < ApplicationRecord
   has_many :companies
   has_many :meeting_rooms, dependent: :destroy
+
+  def residents_nb(companies)
+    residents = 0
+
+    if companies.each do |company|
+      employees = User.where('company_id = ?', company.id)
+      residents += employees.count
+    end
+
+    return residents
+
+  end
+
+
+end
 end
